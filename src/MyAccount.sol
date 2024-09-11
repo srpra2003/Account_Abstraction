4b6e0f9ac07a3f2e15653bc18874f1b77fac4107
sohamkumar <sohamkumar17072003srp@gmail.com>
2024-09-11 10:04:27 +0530

    Created the basic setup for user's smart contract Account

--- src/MyAccount.sol
+++ src/MyAccount.sol
@@ -2,6 +2,30 @@
 
 pragma solidity ^0.8.26;
 
-contract MyAccount {
+import {IAccount} from "lib/account-abstraction/contracts/interfaces/IAccount.sol";
+import {PackedUserOperation} from "lib/account-abstraction/contracts/interfaces/PackedUserOperation.sol";
+
+contract MyAccount is IAccount {
+
+
+    function validateUserOp(
+        PackedUserOperation calldata userOp,
+        bytes32 userOpHash,
+        uint256 missingAccountFunds
+    ) external returns (uint256 validationData) {
+
+    }
+    
+
+    function _validateSignature(
+        PackedUserOperation calldata userOp,
+        bytes32 userOpHash
+    ) internal virtual returns (uint256 validationData) {
+        
+    }
+
+    function _payPrefund(uint256 missingAccountFunds) internal virtual {
+
+    }
     
 }
